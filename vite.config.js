68d73ed911609aa179ce76f655ce855bda55e136
KL2400030537 <2400030537@kluniversity.in>
2025-08-04 12:24:38 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/PROJECT_FE_P004/'
+  base : '/PROJECT_FE_P004/',
+   server : {
+    port : 1001
+  }
 })
